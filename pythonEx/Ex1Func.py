83c99ec4f91ac4cfb2f3b7843c122af06e773117
Ruy <144906321+ruynt@users.noreply.github.com>
2024-10-10 19:12:47 -0300

    Delete pythonEx directory

--- pythonEx/Ex1Func.py
+++
@@ -1,7 +0,0 @@
-def calc_valor(preço, desconto):
-    resp = preço * desconto / 100
-    return resp
-
-def calc_pagar(preço, valordedesconto):
-    resul = preço - valordedesconto
-    return resul
